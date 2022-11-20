721fabb63b3328a21b64b4ca6b8989767d077108
Poonam <poonam.chauhan229@gmail.com>
2022-11-20 13:53:26 +0530

    created an server

---
+++ index.js
@@ -0,0 +1,16 @@
+var express=require('express')
+var ejs=require('ejs')
+
+
+
+var app=express()
+
+// create an server
+
+// http://localhost:8080/
+app.get('/',(req,res)=>{
+    res.send("Welcome to the server")
+})
+app.listen(8080,()=>{
+    console.log("Sever started at port 8080")
+})
